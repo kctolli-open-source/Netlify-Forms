b75e3accb037e92ea47b86d50b54aee85d4fd70e
kctolli <developer@ktolliver.org>
2023-02-08 21:33:43 -0700

    contact

---
+++ react/src/Form.jsx
@@ -0,0 +1,39 @@
+function Form() {
+    return (
+        <form name="connect" method="POST" netlify>
+            <p>
+                <label>Enter Your Name: 
+                <input type="text" name="name" />
+                </label>
+            </p>
+            <p>
+                <label>Enter Your Email: 
+                <input type="email" name="email" />
+                </label>
+            </p>
+            <p>
+                <label>
+                    <input type="radio" name="phonetype" value="Cell" /> Cell
+                </label>
+                <label>
+                    <input type="radio" name="phonetype" value="Work" /> Work
+                </label>
+                <label>
+                    <input type="radio" name="phonetype" value="Home" /> Home
+                </label>
+                <label>
+                    <input type="radio" name="phonetype" value="Other" /> Other
+                </label>
+            </p>
+            <p>
+                <label>Enter Your Phone Number: 
+                <input type="tel" name="phone" />
+                </label>
+            </p>
+            <br />
+            <button type="submit">Submit</button>
+        </form>
+    );
+}
+
+export default Form;
